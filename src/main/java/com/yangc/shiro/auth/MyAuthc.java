99f1580c63ca0b0a1906c1c8900a33ea97191217
yangchenjava <yangchen_java@126.com>
2014-05-20 00:39:31 +0800

    shiro

--- src/main/java/com/yangc/shiro/auth/MyAuthc.java
+++ src/main/java/com/yangc/shiro/auth/MyAuthc.java
@@ -1,5 +1,15 @@
 package com.yangc.shiro.auth;
 
-public class MyAuthc {
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import org.apache.shiro.web.filter.authz.AuthorizationFilter;
+
+public class MyAuthc extends AuthorizationFilter {
+
+	@Override
+	protected boolean isAccessAllowed(ServletRequest request, ServletResponse response, Object mappedValue) throws Exception {
+		return false;
+	}
 
 }
--- src/main/java/com/yangc/shiro/auth/MyRealm.java
+++ src/main/java/com/yangc/shiro/auth/MyRealm.java
@@ -1,5 +1,30 @@
 package com.yangc.shiro.auth;
 
-public class MyRealm {
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+
+import com.yangc.system.service.UserService;
+
+public class MyRealm extends AuthorizingRealm {
+
+	private UserService userService;
+
+	@Override
+	protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
+		return null;
+	}
+
+	@Override
+	protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+		return null;
+	}
+
+	public void setUserService(UserService userService) {
+		this.userService = userService;
+	}
 
 }
